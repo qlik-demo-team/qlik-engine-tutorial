1587e53e0d5f48fc7ad0e0c534e0361e3432328b
Colin McNamara <coolinmc6@gmail.com>
2020-03-05 23:14:40 -0500

    Repo setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import Home from './Home';
+
+const App = () => <Home />
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
