aceff0f9988bfcba8581347dfbd3e8259c14c072
Colin McNamara <coolinmc6@gmail.com>
2020-03-06 00:39:52 -0500

    Hypercube

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,13 @@ const enigma = require('enigma.js');
 const schema = require('enigma.js/schemas/12.20.0.json');
 const SenseUtilities = require('enigma.js/sense-utilities');
 import hypercube from './hypercube';
-import listObject from './listObject';
 
 const config = {
-  host: '', // Qlik Server Domain
+  host: 'sense-demo-staging.qlik.com',
   secure: true,
   port: 443,
   prefix: 'windows',
-  appId: '', // ==> App ID
+  appId: '52b96c54-ca5c-4ec6-a10d-39992c733959', // ==> Consumer Sales
 };
 const url = SenseUtilities.buildUrl(config);
 const qDocPromise = enigma.create({ schema, url }).open().then(global => global.openDoc(config.appId));
@@ -24,18 +23,6 @@ qDocPromise.then((doc) => {
     console.log(dataPages)
   }))
   
-  // Making a Selection with a List Object
-  doc.createSessionObject(listObject).then((obj) => {
-    obj.getListObjectData('/qListObjectDef',[ {qLeft: 0, qTop: 0, qWidth: 10, qHeight: 100}]).then(objectData => {
-      console.log(objectData);
-      obj.selectListObjectValues('/qListObjectDef', [2], true, false).then((res) => {
-        console.log(res);
-        obj.getListObjectData('/qListObjectDef',[ {qLeft: 0, qTop: 0, qWidth: 10, qHeight: 100}]).then(objectData2 => {
-          console.log(objectData2)
-        })
-      })
-    })
-  })
 })
 
 const Home = () => {
